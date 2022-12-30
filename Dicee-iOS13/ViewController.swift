6ce7b446cdc107a04a2b80743d881ccb8d298476
7H3-PR073C70R <okanlawondamilare24434@gmail.com>
2022-12-30 14:21:12 +0200

    animate dice image

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -15,12 +15,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     
     
-    let imageList = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
+    let imageList = [UIImage(named: "DiceOne")!, UIImage(named: "DiceTwo")!, UIImage(named: "DiceThree")!, UIImage(named: "DiceFour")!, UIImage(named: "DiceFive")!, UIImage(named: "DiceSix")!]
 
     @IBAction func rollButtonPressed(_ sender: UIButton){
+        diceImageView1.animationImages = imageList
+        diceImageView2.animationImages = imageList.reversed()
+        diceImageView1.animationDuration = TimeInterval(1)
+        diceImageView2.animationDuration = TimeInterval(1)
+        diceImageView1.startAnimating()
+        diceImageView2.startAnimating()
+        Task {
+            try await Task.sleep(nanoseconds: 1_000_000_000)
+            diceImageView1.stopAnimating()
+            diceImageView2.stopAnimating()
+            diceImageView1.image = imageList[Int.random(in: 0...5)]
+            diceImageView2.image = imageList[Int.random(in: 0...5)]
+        }
         
-        diceImageView1.image = imageList[Int.random(in: 0...5)]
-        diceImageView2.image = imageList[Int.random(in: 0...5)]
     }
     
 }
